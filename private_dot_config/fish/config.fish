46189015052890d30bee7906478eef5a04edd36a
Patrick <petrik09@live.nl>
2025-02-22 17:48:18 +0100

    chore: updated fish and zed config

--- private_dot_config/fish/config.fish
+++ private_dot_config/fish/config.fish
@@ -1,1 +1,22 @@
 set fish_greeting
+
+abbr gc 'git checkout'
+abbr ga 'git add .'
+abbr gs 'git status'
+abbr gst 'git stash'
+abbr gstp 'git stash pop'
+abbr gm 'git commit -m'
+abbr gma 'git commit --amend'
+abbr gf 'git fetch'
+abbr gp 'git pull'
+abbr gP 'git push'
+abbr gl 'git log'
+abbr gd 'git diff'
+abbr gr 'git rebase -i'
+abbr grc 'git rebase --continue'
+abbr gra 'git rebase --abort'
+abbr grs 'git restore'
+abbr grss 'git restore --staged'
+
+abbr dcu 'docker compose up'
+abbr dcr 'docker compose run --rm'
