ac9ff0f337b3e823f335bc1b711b6fc6f8bff285
Patrick <petrik09@live.nl>
2025-03-03 16:12:59 +0100

    docs: removed a few comments

--- dot_config/nvim/lua/plugins/tabline.lua
+++ dot_config/nvim/lua/plugins/tabline.lua
@@ -3,8 +3,8 @@ require("bufferline").setup({
 		offsets = {
 			{ filetype = "NvimTree" },
 		},
-		show_buffer_close_icons = false, -- Use keymap instead.
-		show_close_icon = false, -- Hide close button for tabpages as well
+		show_buffer_close_icons = false,
+		show_close_icon = false,
 	},
 	-- Integrate with colorscheme.
 	highlights = require("catppuccin.groups.integrations.bufferline").get(),
