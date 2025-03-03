5f527b482efa6b89c89e665e68c858dc61408573
Patrick <petrik09@live.nl>
2025-03-03 16:11:45 +0100

    chore: updated nvim config

--- dot_config/nvim/lua/plugins/tabline.lua
+++ dot_config/nvim/lua/plugins/tabline.lua
@@ -4,6 +4,7 @@ require("bufferline").setup({
 			{ filetype = "NvimTree" },
 		},
 		show_buffer_close_icons = false, -- Use keymap instead.
+		show_close_icon = false, -- Hide close button for tabpages as well
 	},
 	-- Integrate with colorscheme.
 	highlights = require("catppuccin.groups.integrations.bufferline").get(),
