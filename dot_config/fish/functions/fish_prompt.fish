bef8603b4b3b02256d6a30a0850fedb04200b40d
Patrick <petrik09@live.nl>
2025-03-23 18:09:35 +0100

    chore: updated fish prompt

--- dot_config/fish/functions/fish_prompt.fish
+++ dot_config/fish/functions/fish_prompt.fish
@@ -1,33 +1,31 @@
 function fish_prompt
     set last_status $status
 
-    set white (set_color normal; set_color white)
-    set red (set_color normal; set_color red)
-    set green (set_color normal; set_color green)
-    set blue (set_color normal; set_color blue)
-    set yellow (set_color normal; set_color yellow)
-
-    echo -n -s $blue " 󰝰 " (prompt_pwd --dir-length=0) " "
+    set cwd (prompt_pwd --dir-length=0)
+    set_color blue
+    echo -n " 󰝰 $cwd "
 
     set is_inside_git_dir (git rev-parse --is-inside-work-tree 2>/dev/null)
 
     if test -n "$is_inside_git_dir"
-        set git_branch (git symbolic-ref --short HEAD 2>/dev/null)
+        set git_branch (git symbolic-ref --short HEAD)
         set is_dirty (git status --porcelain)
 
         if test -n "$is_dirty"
-            set git_color $yellow
+            set_color yellow
         else
-            set git_color $green
+            set_color green
         end
 
-        echo -n -s $git_color "󰘬 " $git_branch " "
+        echo -n "󰘬 $git_branch "
     end
 
     if test $last_status != 0
-        echo -n -s $red "󰅗 "
+        set_color red
+        echo -n "󰅗 "
     else
-        echo -n -s $green "󰧚 "
+        set_color green
+        echo -n "󰧚 "
     end
 
     set_color normal
--- dot_config/fish/functions/fish_right_prompt.fish
+++ dot_config/fish/functions/fish_right_prompt.fish
@@ -1,3 +1,25 @@
 function fish_right_prompt
-    echo -n -s (set_color --dim white) "󰅐 " (date +%T) (set_color normal)
+    set milliseconds $CMD_DURATION
+    set seconds (math "$milliseconds / 1000")
+    set minutes (math "$seconds / 60")
+
+    set_color --dim white
+    echo -n "󰔛 "
+
+    if test $seconds -ge 1
+        if test $minutes -ge 1
+            set remaing_seconds (math "$seconds % 60")
+            echo -n -s $minutes m $remaing_seconds s " "
+        else
+            echo -n -s $seconds s " "
+        end
+    else
+        echo -n -s $milliseconds ms " "
+    end
+
+    set time (date +%T)
+    set_color --dim white
+    echo -n "󰅐 $time"
+
+    set_color normal
 end
