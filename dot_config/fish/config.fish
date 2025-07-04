798ffd27fd8d75946a494117680d0f0b7e08fd5d
Patrick <petrik09@live.nl>
2025-07-04 07:28:19 +0200

    chore: update

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -14,6 +14,7 @@ abbr gstl 'git stash list'
 abbr gstp 'git stash pop'
 abbr gm 'git commit -m'
 abbr gma 'git commit --amend'
+abbr gman 'git commit --amend --no-edit'
 abbr gf 'git fetch'
 abbr gp 'git pull'
 abbr gP 'git push'
@@ -36,5 +37,7 @@ abbr ns 'nix-shell --run fish'
 abbr nr 'sudo nixos-rebuild switch --flake ~/nix#default'
 
 abbr venv 'source .venv/bin/activate.fish'
+
+abbr tr 'tree --gitignore --dirsfirst'
 
 oh-my-posh init fish --config ~/.config/oh-my-posh/config.json | source
