c669c9e0a4c685a584092234b563e1d6d9834276
Patrick <petrik09@live.nl>
2026-04-11 11:37:43 +0200

    chore: update (and cleanup)

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,15 +1,5 @@
 set fish_greeting
 
-set -x FZF_DEFAULT_OPTS "\
-  --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
-  --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
-  --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
-  --color=selected-bg:#45475a \
-  --color=border:#6c7086,label:#cdd6f4"
-
-abbr lg lazygit
-abbr ld lazydocker
-
 abbr gb 'git branch'
 abbr gc 'git switch'
 abbr ga 'git add .'
@@ -39,16 +29,10 @@ abbr dcu 'docker compose up'
 abbr dcr 'docker compose run --rm'
 
 abbr ns 'nix-shell --run fish'
+abbr nc 'sudo nix-collect-garbage -d'
 abbr nb --set-cursor 'sudo nixos-rebuild switch --flake ~/nix#%'
 abbr nr --set-cursor 'nix run nixpkgs#%'
 abbr nq --set-cursor 'nix-store -q --requisites /nix/store/%'
 
-abbr tr 'tree --gitignore --dirsfirst'
-
 abbr venv 'source .venv/bin/activate.fish'
 abbr venvd 'source .venv/bin/deactivate.fish'
-
-oh-my-posh init fish --config ~/.config/oh-my-posh/config.json | source
-
-fzf --fish | source
-
