a829e69b3399ecaad4e62eef84629ff262bacea3
Patrick <petrik09@live.nl>
2026-06-06 18:35:44 +0200

    chore: update

--- dot_config/exact_nvim/exact_lua/exact_config/exact_core/keymaps.lua
+++ dot_config/exact_nvim/exact_lua/exact_config/exact_core/keymaps.lua
@@ -1,14 +1,10 @@
--- Pickers
 vim.keymap.set('n', '<leader>f', '<cmd>FZF<cr>', { desc = "Open file picker" })
 
--- Clipboard
 vim.keymap.set({ 'n', 'v' }, '<leader>y', '"+y', { desc = "Yank to system clipboard" })
 vim.keymap.set({ 'n', 'v' }, '<leader>p', '"+p', { desc = "Paste from system clipboard" })
 vim.keymap.set({ 'n', 'v' }, '<leader>P', '"+P', { desc = "Paste from system clipboard" })
 
--- Replace
 vim.keymap.set('v', 'R', '"_dP', { desc = 'Replace selection with yanked text' })
 vim.keymap.set('v', '<leader>R', '"_d"+P', { desc = 'Replace selection with system clipboard' })
 
--- Misc
 vim.keymap.set('n', '<esc>', '<cmd>nohl<cr>', { desc = "Clear highlight" })
