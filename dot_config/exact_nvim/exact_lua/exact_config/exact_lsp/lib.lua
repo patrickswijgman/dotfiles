c17611a791136a7c69aa1892e3f449749c1aa82f
Patrick <petrik09@live.nl>
2026-06-09 19:45:50 +0200

    chore: update

--- dot_config/exact_nvim/exact_lua/exact_config/exact_lsp/lib.lua
+++ dot_config/exact_nvim/exact_lua/exact_config/exact_lsp/lib.lua
@@ -4,13 +4,11 @@ local M = {}
 
 function M.client_request(client, method, params, bufnr)
   local result = client:request_sync(method, params, consts.REQUEST_TIMEOUT, bufnr)
-
   if result == nil then
     vim.notify(("[LSP] %s: %s failed"):format(client.name, method), vim.log.levels.ERROR)
   elseif result.err then
     vim.notify(("[LSP] %s: %s (%s)"):format(client.name, result.err.message, result.err.data), vim.log.levels.ERROR)
   end
-
   return result and result.result
 end
 
