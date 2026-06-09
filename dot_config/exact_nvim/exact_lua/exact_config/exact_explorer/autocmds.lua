8686abe17f23f97920c10096834f62d9fc84f685
Patrick <petrik09@live.nl>
2026-06-09 19:44:21 +0200

    chore: update

--- dot_config/exact_nvim/exact_lua/exact_config/exact_explorer/autocmds.lua
+++ dot_config/exact_nvim/exact_lua/exact_config/exact_explorer/autocmds.lua
@@ -1,6 +1,6 @@
 local lib = require("config.explorer.lib")
 
-local group = vim.api.nvim_create_augroup("UserExplorer", { clear = true })
+local group = vim.api.nvim_create_augroup("UserExplorerConfig", { clear = true })
 
 vim.api.nvim_create_autocmd("VimResized", {
   callback = function()
