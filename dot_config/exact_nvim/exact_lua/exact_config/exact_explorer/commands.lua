12b3859a8f258e51ab852ff2f518ce6e2ee5d0d6
Patrick <petrik09@live.nl>
2026-06-09 20:38:23 +0200

    chore: update

--- dot_config/exact_nvim/exact_lua/exact_config/exact_explorer/commands.lua
+++ dot_config/exact_nvim/exact_lua/exact_config/exact_explorer/commands.lua
@@ -3,5 +3,5 @@ local lib = require("config.explorer.lib")
 vim.api.nvim_create_user_command("Explorer", function()
   lib.toggle()
 end, {
-  desc = "Open file explorer in a floating window",
+  desc = "Toggle file explorer",
 })
