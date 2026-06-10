5536028ee6821ea42cf92041854b7ce1e723c523
Patrick <petrik09@live.nl>
2026-06-10 21:29:58 +0200

    chore: update

--- dot_config/exact_nvim/exact_lua/exact_config/exact_core/keymaps.lua
+++ dot_config/exact_nvim/exact_lua/exact_config/exact_core/keymaps.lua
@@ -1,3 +1,8 @@
+-- Plugins
+vim.keymap.set("n", "<leader>e", ":Explorer<cr>", { desc = "Open explorer" })
+vim.keymap.set("n", "<leader>f", ":Files ", { desc = "Search for files" })
+vim.keymap.set("n", "<leader>g", ":Grep ", { desc = "Grep in files" })
+
 -- Clipboard
 vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "Yank to system clipboard" })
 vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { desc = "Paste from system clipboard" })
@@ -7,10 +12,5 @@ vim.keymap.set({ "n", "v" }, "<leader>P", '"+P', { desc = "Paste from system cli
 vim.keymap.set("v", "R", '"_dP', { desc = "Replace selection with yanked text" })
 vim.keymap.set("v", "<leader>R", '"_d"+P', { desc = "Replace selection with system clipboard" })
 
--- Plugins
-vim.keymap.set("n", "<leader>e", "<cmd>Explorer<cr>", { desc = "Open explorer" })
-vim.keymap.set("n", "<leader>f", ":Files ", { desc = "Search for files" })
-vim.keymap.set("n", "<leader>g", ":Grep ", { desc = "Grep in files" })
-
 -- Misc
-vim.keymap.set("n", "<esc>", "<cmd>nohl<cr>", { desc = "Clear highlight" })
+vim.keymap.set("n", "<esc>", ":nohl<cr>", { desc = "Clear highlight" })
