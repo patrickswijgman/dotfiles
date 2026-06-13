cc1d9379b15a0555fabb8441190db9d420268b76
Patrick <petrik09@live.nl>
2026-06-13 20:41:53 +0200

    chore: update

--- dot_config/exact_nvim/exact_lua/exact_config/autocmds.lua
+++ dot_config/exact_nvim/exact_lua/exact_config/autocmds.lua
@@ -13,3 +13,12 @@ vim.api.nvim_create_autocmd("BufWritePre", {
   desc = "Remove trailing whitespaces before writing the buffer",
   group = group,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "qf",
+  callback = function(ev)
+    vim.keymap.set("n", "q", ":cclose<cr>", { buffer = ev.buf, desc = "Close quickfix list" })
+  end,
+  desc = "Quickfix list keymaps",
+  group = group,
+})
--- dot_config/exact_nvim/exact_lua/exact_config/keymaps.lua
+++ dot_config/exact_nvim/exact_lua/exact_config/keymaps.lua
@@ -1,3 +1,13 @@
+local function toggle_quickfix_list()
+  local list = vim.fn.getqflist({ winid = 0 })
+
+  if list.winid ~= 0 then
+    vim.cmd("cclose")
+  else
+    vim.cmd("copen")
+  end
+end
+
 vim.keymap.set("n", "<leader>e", ":Explorer<cr>", { desc = "Open explorer" })
 vim.keymap.set("n", "<leader>f", ":Files ", { desc = "Search for files" })
 vim.keymap.set("n", "<leader>g", ":Grep ", { desc = "Grep in files" })
@@ -6,17 +16,10 @@ vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "Yank to system clipbo
 vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { desc = "Paste from system clipboard" })
 vim.keymap.set({ "n", "v" }, "<leader>P", '"+P', { desc = "Paste from system clipboard" })
 
-vim.keymap.set("n", "<leader>q", ":copen<cr>", { desc = "Open quickfix list" })
-vim.keymap.set("n", "<c-j>", ":cnext<cr>zz", { desc = "Previous quickfix item and center view" })
-vim.keymap.set("n", "<c-k>", ":cprev<cr>zz", { desc = "Next quickfix item and center view" })
+vim.keymap.set("n", "<leader>q", toggle_quickfix_list, { desc = "Toggle quickfix list" })
 
 vim.keymap.set("v", "R", '"_dP', { desc = "Replace selection with yanked text" })
 vim.keymap.set("v", "<leader>R", '"_d"+P', { desc = "Replace selection with system clipboard" })
-
-vim.keymap.set("n", "<c-d>", "<c-d>zz", { desc = "Scroll down and center view" })
-vim.keymap.set("n", "<c-u>", "<c-u>zz", { desc = "Scroll up and center view" })
-vim.keymap.set("n", "n", "nzzzv", { desc = "Find next pattern, center view and open fold" })
-vim.keymap.set("n", "N", "Nzzzv", { desc = "Find previous pattern, center view and open fold" })
 
 vim.keymap.set("n", "<esc>", ":nohl<cr>", { desc = "Clear highlight", silent = true })
 
