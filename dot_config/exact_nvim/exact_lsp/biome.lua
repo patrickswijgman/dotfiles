de5fad4388f68e80a076d80f06dea4c2edf29971
Patrick <petrik09@live.nl>
2026-06-13 10:28:44 +0200

    chore: update

--- dot_config/exact_nvim/exact_lsp/biome.lua
+++ dot_config/exact_nvim/exact_lsp/biome.lua
@@ -2,18 +2,14 @@
 return {
   cmd = { "npx", "biome", "lsp-proxy" },
   filetypes = {
-    "astro",
     "css",
-    "graphql",
     "html",
     "javascript",
     "javascriptreact",
     "json",
     "jsonc",
-    "svelte",
     "typescript",
     "typescriptreact",
-    "vue",
   },
   root_markers = {
     "biome.json",
--- dot_config/exact_nvim/exact_lua/exact_config/keymaps.lua
+++ dot_config/exact_nvim/exact_lua/exact_config/keymaps.lua
@@ -7,9 +7,16 @@ vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { desc = "Paste from system cli
 vim.keymap.set({ "n", "v" }, "<leader>P", '"+P', { desc = "Paste from system clipboard" })
 
 vim.keymap.set("n", "<leader>q", ":copen<cr>", { desc = "Open quickfix list" })
+vim.keymap.set("n", "<c-j>", ":cnext<cr>zz", { desc = "Previous quickfix item and center view" })
+vim.keymap.set("n", "<c-k>", ":cprev<cr>zz", { desc = "Next quickfix item and center view" })
 
 vim.keymap.set("v", "R", '"_dP', { desc = "Replace selection with yanked text" })
 vim.keymap.set("v", "<leader>R", '"_d"+P', { desc = "Replace selection with system clipboard" })
+
+vim.keymap.set("n", "<c-d>", "<c-d>zz", { desc = "Scroll down and center view" })
+vim.keymap.set("n", "<c-u>", "<c-u>zz", { desc = "Scroll up and center view" })
+vim.keymap.set("n", "n", "nzzzv", { desc = "Find next pattern, center view and open fold" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Find previous pattern, center view and open fold" })
 
 vim.keymap.set("n", "<esc>", ":nohl<cr>", { desc = "Clear highlight", silent = true })
 
