a246d205ca128017fcb10c66dd92ae8a96049beb
Patrick <petrik09@live.nl>
2026-06-17 18:31:31 +0200

    chore: rust analyzer

--- dot_config/exact_nvim/exact_lsp/rust_analyzer.lua
+++ dot_config/exact_nvim/exact_lsp/rust_analyzer.lua
@@ -8,33 +8,4 @@ return {
     "Cargo.toml",
     ".git",
   },
-  capabilities = {
-    experimental = {
-      serverStatusNotification = true,
-      commands = {
-        commands = {
-          "rust-analyzer.showReferences",
-          "rust-analyzer.runSingle",
-          "rust-analyzer.debugSingle",
-        },
-      },
-    },
-  },
-  settings = {
-    ["rust-analyzer"] = {
-      lens = {
-        debug = { enable = true },
-        enable = true,
-        implementations = { enable = true },
-        references = {
-          adt = { enable = true },
-          enumVariant = { enable = true },
-          method = { enable = true },
-          trait = { enable = true },
-        },
-        run = { enable = true },
-        updateTest = { enable = true },
-      },
-    },
-  },
 }
