77e18e5b58a09617fbbf35844bf52e24df6730e0
Patrick <petrik09@live.nl>
2026-06-20 09:40:48 +0200

    refactor: dotlink script use fd

--- .config/fish/functions/dotlink.fish
+++ .config/fish/functions/dotlink.fish
@@ -1,15 +1,22 @@
 function dotlink --description "Symlink ~/dotfiles into home directory"
-    set repo ~/dotfiles
-    set files (find $repo -not -path '*/.git/*' -type f | sort)
+    set repo $HOME/dotfiles
+    set ignore .git README.md
 
-    for src in $files
-        set rel (string replace "$repo/" "" $src)
-        set dst "$HOME/$rel"
+    set fd_args --base-directory $repo --type file --hidden
+    for pattern in $ignore
+        set fd_args $fd_args --exclude $pattern
+    end
+
+    echo "Applying dotfiles:"
+
+    for file in (fd $fd_args)
+        set src $repo/$file
+        set dst $HOME/$file
 
         mkdir -p (dirname $dst)
         ln -sf $src $dst
 
-        echo $rel
+        echo $src "→" $dst
     end
 end
 
