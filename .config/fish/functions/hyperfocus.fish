43ebd0a591c23c975268648f0d9d87a083ee8508
Patrick <petrik09@live.nl>
2026-06-28 08:28:12 +0200

    feat(claude): grilling skill

--- .config/fish/functions/hyperfocus.fish
+++ .config/fish/functions/hyperfocus.fish
@@ -1,6 +1,7 @@
 function hyperfocus --description 'Lock screen after a while to break hyperfocus; restarts on unlock'
     while true
         sleep 30m
+        playerctl pause
         swaylock
     end
 end
