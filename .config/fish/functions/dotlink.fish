458f2008495b4294d58d847d5598f3a229516bc0
Patrick <petrik09@live.nl>
2026-07-01 15:27:45 +0200

    chore: update

--- .config/fish/functions/dotlink.fish
+++ .config/fish/functions/dotlink.fish
@@ -1,23 +1,33 @@
 function dotlink --description "Symlink ~/dotfiles into home directory"
     set repo $HOME/dotfiles
     set ignore .git README.md
+    set delete $HOME/.config/nvim
 
     set fd_args --base-directory $repo --type file --type symlink --hidden
     for pattern in $ignore
         set fd_args $fd_args --exclude $pattern
     end
 
+    echo "Deleting files and directories:"
+
+    for path in $delete
+        rm -rf $path
+        echo $path
+    end
+
+    echo ""
     echo "Applying dotfiles:"
 
     for file in (fd $fd_args)
         set src $repo/$file
         set dst $HOME/$file
-
         mkdir -p (dirname $dst)
         ln -sf $src $dst
-
         echo $file
     end
+
+    echo ""
+    echo "Done!"
 end
 
 if not status is-interactive
--- .config/nvim/lua/pack.lua
+++ .config/nvim/lua/pack.lua
@@ -7,6 +7,5 @@ vim.pack.add({
   "https://github.com/nvim-treesitter/nvim-treesitter-textobjects",
   "https://github.com/patrickswijgman/butter.nvim",
   "https://github.com/stevearc/conform.nvim",
-  "https://github.com/stevearc/oil.nvim",
   "https://github.com/vague-theme/vague.nvim",
 })
