8df6494c0287b3cecc505c4757df94c3db31ed91
Patrick <petrik09@live.nl>
2026-07-07 19:48:04 +0200

    chore: update

--- .config/fish/functions/hyperfocus.fish
+++ .config/fish/functions/hyperfocus.fish
@@ -1,7 +1,6 @@
 function hyperfocus --description 'Lock screen after a while to break hyperfocus; restarts on unlock'
     while true
         sleep 30m
-        playerctl pause
         swaylock
     end
 end
