452f1e79ecf291e95b939702c0cc4798900e284e
Patrick <petrik09@live.nl>
2026-07-10 08:47:59 +0200

    chore: update

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -1,10 +1,23 @@
 config.load_autoconfig(False)
 
+# Appearance
+
 config.source('themes/gruvbox-material.py')
+
+# Settings
 
 c.auto_save.session = True
 c.colors.webpage.preferred_color_scheme = "dark"
 c.content.fullscreen.overlay_timeout = 0
 c.content.fullscreen.window = True
-c.editor.command = ['xdg-terminal-exec', 'nvim', '{file}', '-c', 'normal {line}G{column0}l']
 c.scrolling.smooth = True
+
+# Permissions
+
+c.content.javascript.clipboard = "access"
+c.content.notifications.enabled = False
+c.content.notifications.presenter = "libnotify"
+
+# Grant permissions to Google Calendar without prompting
+config.set('content.notifications.enabled', True, 'https://calendar.google.com')
+config.set('content.register_protocol_handler', True, 'https://calendar.google.com')
