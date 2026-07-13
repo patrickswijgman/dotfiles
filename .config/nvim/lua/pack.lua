bffe202693f533ef6cce9c5f840a60d4486bc76b
Patrick <petrik09@live.nl>
2026-07-13 11:50:13 +0200

    chore: update

--- .config/nvim/lua/pack.lua
+++ .config/nvim/lua/pack.lua
@@ -11,7 +11,10 @@ vim.pack.add({
   { src = "https://github.com/patrickswijgman/birb.nvim" },
   { src = "https://github.com/patrickswijgman/bulb.nvim" },
   { src = "https://github.com/patrickswijgman/butter.nvim" },
-  { src = "https://github.com/sainnhe/gruvbox-material" },
   { src = "https://github.com/stevearc/conform.nvim" },
   { src = "https://github.com/windwp/nvim-autopairs" },
+
+  -- Colorschemes
+  { src = "https://github.com/sainnhe/gruvbox-material" },
+  { src = "https://github.com/felipeagc/fleet-theme-nvim" },
 })
